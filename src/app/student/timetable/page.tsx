c425077cc9a6922d758a515f7151f4922e93be7b
nebil esmael <nebilesmaelprog@gmail.com>
2025-12-11 23:55:13 +0300

    [feat]-timetablefun

---
+++ src/app/student/timetable/page.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/components/layout/dashboard-layout'
+import React from 'react'
+
+const  StudentTimeTablePage= () => {
+  return (
+    <DashboardLayout>
+        <div></div>
+    </DashboardLayout>
+  )
+}
+
+export default StudentTimeTablePage
